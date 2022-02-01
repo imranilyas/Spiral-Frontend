e643ccf2b34b9b9866c8937c3d8ac2f1f73d623f
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-02-01 00:31:17 -0800

    Basic Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div className="App">
-      Welcome to Spiral Banking App
+      <Router basename='/'>
+        <Routes>
+          <Route index element = {<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
