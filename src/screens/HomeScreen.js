af1c8a97f25ab6e16c43bf32db24376d645086f3
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-02-02 14:10:20 -0800

    Update HomeScreen.js
    keeping track of which components will be active

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,32 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-import NavDisplay from "../components/NavDisplay";
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import NavDisplay from "./NavDisplay";
 
 import './Screens.css';
 
 const HomeScreen = () => {
+
+    const [role] = useState("Account Holder");
+    const [activeComponent, setActiveComponent] = useState("");
+
+    const activeHandler = (element) => {
+        setActiveComponent(element);
+    }
+
+    // Loading up components
+    useEffect(() => {
+        console.log("Inside the useEffect");
+        if(role === "Account Holder") {
+            setActiveComponent("Accounts");
+        } else {
+            setActiveComponent("Create");
+        }
+    }, [role]);
+
     return (
         <div className="HomeScreen">
-            <Navbar />
-            <NavDisplay />
+            <Navbar role = {role} active = {activeHandler} />
+            <NavDisplay active = {activeComponent} />
         </div>
     );
 }
