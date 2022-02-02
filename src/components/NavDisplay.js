32b67a0aa63b80246c65c303ea3d6022da5cd5a9
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-02-01 20:03:41 -0800

    Initial Components for Logged in Users
    basic css that specifies the boundaries of each part of the screen

---
+++ src/components/NavDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavDisplay = () => {
+    return (
+        <div className = "NavDisplay">
+            Something dumber    
+        </div>
+    );
+}
+
+export default NavDisplay;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className = "Navbar">
+            Something dumb
+        </div>
+    );
+}
+
+export default Navbar;
