7b84b390ab16208d446e7778500ee63cef2e1d73
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-02-02 11:08:57 -0800

    Init Navbar
    basic layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 const Navbar = () => {
+    //! Conditionally Render Create Customer Component...except Sign Out
     return (
         <div className = "Navbar">
-            Something dumb
+            <img className="SpiralLogoHome" src = "/spiral-name.png" alt = "Spiral Name"/>
+            <div>
+                {/* Some sort of welcome here */}
+                <h2>Spiral Banking</h2>
+            </div>
+            <div className = "Navlinks">
+                <p>Create New Account</p>
+                <p>Accounts</p> {/* //! Make transactions be found after clicking on each account !\\ */}
+                <p>Withdrawl / Deposit</p>
+                <p>Transfer</p>
+                <p>Sign Out</p>
+            </div>
         </div>
     );
 }
