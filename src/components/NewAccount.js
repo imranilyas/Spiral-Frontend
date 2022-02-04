3e536eb167ac8da4b4c271623a152f0cb3f337b7
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-02-03 17:59:25 -0800

    Check Pan in New Account
    added some styling and state management for a personal account number

--- src/components/NewAccount.js
+++ src/components/NewAccount.js
@@ -1,11 +1,37 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 import './Components.css';
 
 const NewAccount = () => {
+
+    const [pan, setPan] = useState("");
+    const [panExists, setPanExists] = useState("❌");
+
+    //! Call Axios to check if PAN exists
+    const checkPan = () => {
+        console.log(pan);
+        if(pan) {
+            setPanExists("✅ Account exists");
+        } else {
+            setPanExists("❌ No account exists");
+        }
+    }
+
     return (
         <div className="NewAccount">
-            
+            <div>
+                <h1>Create a New Account</h1>
+            </div>
+            <div>
+                <label>Personal Account Number:</label>
+                <input type="number" value = {pan} onChange={e => setPan(e.target.value)}/>
+                <input type = "submit" value = "Check" onClick={checkPan} />
+                <label>{panExists}</label>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 }
