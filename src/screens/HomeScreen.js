92d5230eec5cb841176bc4e16c375a01e7638eb5
imranilyas <68575807+imranilyas@users.noreply.github.com>
2022-03-23 14:31:56 -0700

    Update HomeScreen.js
    opening and closing modal, added backdrop and modal component in home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
+import Backdrop from "../notifications/Backdrop";
+import Modal from "../notifications/Modal";
 import Navbar from "./Navbar";
 import NavDisplay from "./NavDisplay";
 
 import './Screens.css';
 
 const HomeScreen = () => {
+
+    // Modal and Backdrop State
+    const [closingModal, setClosingModal] = useState(false);
+
+    // Opening and Closing Modal and Backdrop
+    const modalAndBackdropHandler = (input) => {
+        setClosingModal(input);
+    }
+
     //! Will change this such that login will pass props here
     const [role] = useState("Account Holder");
     const [activeComponent, setActiveComponent] = useState("");
@@ -25,8 +36,10 @@ const HomeScreen = () => {
 
     return (
         <div className="HomeScreen">
+            <Backdrop backdropState = {closingModal} />
+            <Modal modalState = {closingModal} closeModal = {modalAndBackdropHandler} />
             <Navbar role = {role} active = {activeComponent} activeHandler = {activeHandler} />
-            <NavDisplay active = {activeComponent} />
+            <NavDisplay active = {activeComponent} modal = {modalAndBackdropHandler} />
         </div>
     );
 }
